a7c73cd07541367146e3d08d0dd8179fe402df11
stamovm <m.stamov@yahoo.com>
2022-06-20 23:02:23 -0700

    Created a middleware function to log the api requests.

--- index.js
+++ index.js
@@ -1,32 +1,42 @@
-'use strict'
-const express = require('express')
-const fs = require('fs')
+import express from 'express';
+import * as fs from 'fs';
+import dataSample from './data/dataSample.js';
 
-const app = express()
-const PORT = 5000
-const QUIZ_FILE = './data/quizdata.json'
+const app = express();
+const PORT = 5000;
+const QUIZ_FILE = './data/quizdata.json';
 
 function writeObject(obj, file) {
-  fs.writeFileSync(file, JSON.stringify(obj))
+  fs.writeFileSync(file, JSON.stringify(obj));
 }
 function readObject(file) {
-  return JSON.parse(fs.readFileSync(file))
+  return JSON.parse(fs.readFileSync(file));
 }
 
-const myQuizzes = readObject(QUIZ_FILE)
+const myQuizzes = readObject(QUIZ_FILE);
+
+app.use(function (req, res, next) {
+  console.log(
+    new Date().toLocaleString(),
+    req.method,
+    req.path + ' - ' + req.ip
+  );
+  next();
+});
 
 app.get('/status', (req, res) => {
-  res.send('Server is running on port ' + PORT)
-})
+  res.send('Server is running on port ' + PORT);
+});
 
 app.get('/api/quizzes', (req, res) => {
-  res.json(myQuizzes)
-})
+  console.log('sending data!');
+  res.json(myQuizzes);
+});
 
-// writeObject(myQuizzes,QUIZ_FILE)
-
-console.log(myQuizzes)
+// console.log(dataSample);
+// writeObject(dataSample, QUIZ_FILE);
+// console.log(myQuizzes);
 
 app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`)
-})
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
